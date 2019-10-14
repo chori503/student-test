39dda3313b8edd44c9bb7fa6f3d756f8e9f07349
Jorge <jorgearomero123@gmail.com>
2019-10-14 11:20:28 -0600

    Creacion del Ws con feign Client

---
+++ fClientStudents/src/main/java/com/fClientStudents/FClientStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.fClientStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FClientStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FClientStudentsApplication.class, args);
+	}
+
+}
